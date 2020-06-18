1c629a897894b0eeba26cef5c623ad7ffbdf975c
luffyfight <1161074235@qq.com>
2020-06-18 15:08:46 +0800

    6.18

---
+++ code_150Evaluate Reverse Polish Notation/src/cn/kobe/Solution2.java
@@ -0,0 +1,32 @@
+package cn.kobe;
+
+class Solution2 {
+    //纯数组模拟栈实现(推荐)   3 ms	36 MB
+    public static int evalRPN(String[] tokens) {
+        int[] numStack = new int[tokens.length / 2 + 1];
+        int index = 0;
+        for (String s : tokens) {
+            switch (s) {
+                case "+":
+                    numStack[index - 2] += numStack[--index];
+                    break;
+                case "-":
+                    numStack[index - 2] -= numStack[--index];
+                    break;
+                case "*":
+                    numStack[index - 2] *= numStack[--index];
+                    break;
+                case "/":
+                    numStack[index - 2] /= numStack[--index];
+                    break;
+                default:
+                    // numStack[index++] = Integer.valueOf(s);
+                    //valueOf改为parseInt，减少自动拆箱装箱操作
+                    numStack[index++] = Integer.parseInt(s);
+                    break;
+            }
+        }
+        return numStack[0];
+    }
+}
+
