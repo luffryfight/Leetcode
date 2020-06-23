30c814961c3b32d0e8d430810e95e40c237ad366
luffyfight <1161074235@qq.com>
2020-06-23 18:13:25 +0800

    6.23

---
+++ code_225Implement Stack using Queues/src/cn/kobe/Solution.java
@@ -0,0 +1,35 @@
+package cn.kobe;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class Solution {
+    Deque<Integer> queue=new ArrayDeque<>();
+    /** Push element x onto stack. */
+    public void push(int x) {
+        queue.push(x);
+    }
+
+    /** Removes the element on top of the stack and returns that element. */
+    public int pop() {
+        for (int i = 0; i < queue.size(); i++) {
+            queue.push(queue.pollFirst());
+        }
+        return queue.pollFirst();
+    }
+
+    /** Get the top element. */
+    public int top() {
+        for (int i = 0; i < queue.size(); i++) {
+            queue.push(queue.pollFirst());
+        }
+        int res=queue.pollFirst();
+        queue.push(res);
+        return res;
+    }
+
+    /** Returns whether the stack is empty. */
+    public boolean empty() {
+        return queue.isEmpty();
+    }
+}
