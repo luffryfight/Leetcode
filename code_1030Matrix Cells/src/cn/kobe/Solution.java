10765a7173bd9c811af1944ec45ebccb76792e55
luffyfight <1161074235@qq.com>
2020-11-17 23:03:53 +0800

    11.17

---
+++ code_1030Matrix Cells/src/cn/kobe/Solution.java
@@ -0,0 +1,30 @@
+package cn.kobe;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 作用：练习Compartor接口
+ * 2020/11/17
+ */
+public class Solution {
+    public int[][] allCellsDistOrder(int R, int C, int r0, int c0) {
+        int[][] res=new int[R*C][];
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                res[i*C+j]=new int[]{i,j};
+            }
+        }
+
+        Arrays.sort(res, new Comparator<int[]>(){
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return (Math.abs(r0-o1[0])+Math.abs(c0-o1[1]))-
+                        (Math.abs(r0-o2[0])+Math.abs(c0-o2[1]));
+            }
+        });
+        return res;
+    }
+}
---
+++ code_1030Matrix Cells/src/cn/kobe/Solution2.java
@@ -0,0 +1,37 @@
+package cn.kobe;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 作用：其实就是简单的思路，按照大小放入，并且每个dis可能对应多个值，所以多层list<></>
+ * 2020/11/17
+ */
+class Solution2 {
+    public int[][] allCellsDistOrder(int R, int C, int r0, int c0) {
+        int maxDist = Math.max(r0, R - 1 - r0) + Math.max(c0, C - 1 - c0);
+        List<List<int[]>> bucket = new ArrayList<List<int[]>>();
+        for (int i = 0; i <= maxDist; i++) {
+            bucket.add(new ArrayList<int[]>());
+        }
+
+        for (int i = 0; i < R; i++) {
+            for (int j = 0; j < C; j++) {
+                int d = dist(i, j, r0, c0);
+                bucket.get(d).add(new int[]{i, j});
+            }
+        }
+        int[][] ret = new int[R * C][];
+        int index = 0;
+        for (int i = 0; i <= maxDist; i++) {
+            for (int[] it : bucket.get(i)) {
+                ret[index++] = it;
+            }
+        }
+        return ret;
+    }
+
+    public int dist(int r1, int c1, int r2, int c2) {
+        return Math.abs(r1 - r2) + Math.abs(c1 - c2);
+    }
+}
